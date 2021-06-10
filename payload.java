f75fc58db8df1afae4803d27ae9ac09512940797
ramv0305github <85611845+ramv0305github@users.noreply.github.com>
2021-06-10 13:59:57 +0530

    Commit Payload

---
+++ payload.java
@@ -0,0 +1,16 @@
+
+public class payload {
+
+	public static String AddPlace() {
+		return "{\r\n" + "\r\n" + "    \"location\":{\r\n" + "\r\n" + "        \"lat\" : -38.383494,\r\n" + "\r\n"
+				+ "        \"lng\" : 33.427362\r\n" + "\r\n" + "    },\r\n" + "\r\n" + "    \"accuracy\":50,\r\n"
+				+ "\r\n" + "    \"name\":\"Return fest pro\",\r\n" + "\r\n"
+				+ "    \"phone_number\":\"(+91) 988 894 4747\",\r\n" + "\r\n"
+				+ "    \"address\" : \"49, side layout, cohen 10\",\r\n" + "\r\n"
+				+ "    \"types\": [\"shoe park\",\"shop\"],\r\n" + "\r\n"
+				+ "    \"website\" : \"http://google.com\",\r\n" + "\r\n" + "    \"language\" : \"French-IN\"\r\n"
+				+ "}\r\n" + "";
+	}
+
+}
+ 
